92211d5c89887316ff2a0c7fc1454b9d890609cb
nguye374 <nguye374@sheridancollege.ca>
2021-11-30 01:17:41 -0500

    Nov 30 using useState, useEffect and eventListener to change the behavior of the layout when the orientation change

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -37,10 +37,27 @@ const GameScreen = (props) => {
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
+  const [deviceWidth, setDeviceWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [deviceHeight, setDeviceHeight] = useState(
+    Dimensions.get("window").height
+  );
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
   const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setDeviceWidth(Dimensions.get("window").width);
+      setDeviceHeight(Dimensions.get("window").height);
+    };
+    Dimensions.addEventListener("change", updateLayout);
+    return () => {
+      Dimensions.removeEventListener("change", updateLayout);
+    };
+  });
 
   useEffect(() => {
     if (currentGuess === userChoice) {
@@ -82,8 +99,44 @@ const GameScreen = (props) => {
 
   let listContainerStyle = styles.listContainer;
 
-  if (Dimensions.get("window").width < 350) {
+  if (deviceWidth < 350) {
     listContainerStyle = styles.listContainerBig;
+  }
+
+  if (deviceHeight < 500) {
+    return (
+      <View style={styles.screen}>
+        <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+        <Card style={styles.controls}>
+          <StartButton
+            style={styles.button}
+            onPress={nextGuessHandler.bind(this, "lower")}
+          >
+            <Ionicons name="md-remove" size={24} color="white" />
+          </StartButton>
+          <NumberContainer>{currentGuess}</NumberContainer>
+          <StartButton
+            style={styles.button}
+            onPress={nextGuessHandler.bind(this, "greater")}
+          >
+            <Ionicons name="md-add" size={24} color="white" />
+          </StartButton>
+        </Card>
+        <View style={styles.listContainer}>
+          {/* <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView> */}
+          <FlatList
+            keyExtractor={(item) => item}
+            data={pastGuesses}
+            renderItem={renderListItem.bind(this, pastGuesses.length)}
+            contentContainerStyle={styles.list}
+          />
+        </View>
+      </View>
+    );
   }
   return (
     <View style={styles.screen}>
@@ -128,10 +181,16 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
+  },
+  controls: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "80%",
   },
   listContainer: {
     // width: Dimensions.get("window").width > 500 ? "60%" : "80%",
