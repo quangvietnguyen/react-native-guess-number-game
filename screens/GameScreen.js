6790d8b8a56b3e9779d1f944638d39f607dcb384
nguye374 <nguye374@sheridancollege.ca>
2021-11-30 15:00:03 -0500

    Nov 30 add the Platform

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -15,6 +15,7 @@ import StartButton from "../components/StartButton";
 import { Ionicons } from "@expo/vector-icons";
 import BodyText from "../components/BodyText";
 import Colors from "../constants/colors";
+import { ScreenOrientation } from "expo";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -34,6 +35,8 @@ const renderListItem = (listLength, itemData) => (
 );
 
 const GameScreen = (props) => {
+  // ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  // ScreenOrientation.addOrientationChangeListener();
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
